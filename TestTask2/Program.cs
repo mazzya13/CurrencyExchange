dbc3a2b9908c8568b69d1e3c2f4b06e38bda3ea0
mazzya13 _ <bakurov2011@gmail.com>
2020-09-25 16:12:25 +0500

    Добавьте файлы проекта.

---
+++ TestTask2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace TestTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdoc = XDocument.Load("http://www.cbr.ru/scripts/XML_daily.asp");
+            foreach (XElement Element in xdoc.Element("ValCurs").Elements("Valute"))
+            {
+                XAttribute ID = Element.Attribute("ID");
+                XElement Code = Element.Element("CharCode");
+                XElement Nominal = Element.Element("Nominal");
+                XElement Value = Element.Element("Value");
+
+                int IDs = int.Parse(Regex.Replace(ID.Value, @"\D", ""));
+
+
+                if (ID != null && Code != null && Nominal != null && Value != null && IDs == 01200 )
+                {
+                    decimal ValueHK = decimal.Parse(Value.Value);
+                    decimal NominalHk = decimal.Parse(Nominal.Value);
+                    ValueHK /= NominalHk;
+                    Console.WriteLine($"Курс Гонконского Доллара к Рублю равен: {ValueHK} - 1");
+                }
+            }
+
+                Console.ReadKey();
+
+        }
+    }
+}
